545e90b20c6eb0403c8249fed82533757f09defb
dwg <arni.dwg@gmail.com>
2009-10-03 15:54:13 +0000

    support relative paths

--- src/dialog/options.js
+++ src/dialog/options.js
@@ -5,9 +5,9 @@
  *  Overridable by defining `DialogOptions` before loading the protototo library.
 **/
 Dialog.Options = Object.extend({
-	assetPrefix: '',
-	stylesheetPath: '/stylesheets/protototo/dialogs.css',
-	busyImage: '/images/protototo/loading.gif',
+	assetPrefix: '/',
+	stylesheetPath: 'stylesheets/protototo/dialogs.css',
+	busyImage: 'images/protototo/loading.gif',
 	overlayOpacity: 0.75,
 	transitionDuration: 0.4,
 	defaultWidth: 400
